0719149549f102a5d4004cf7ed61af66da19c1de
Ana Carolina Porto Avila <anacarolinapavila@gmail.com>
2026-09-16 15:03:11 -0300

    Criando serializer d Tag

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from .models import Produto 
+from .models import Produto, Tag
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
